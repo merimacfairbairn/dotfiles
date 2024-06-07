bc71bbefa3b977a75afaf051edd21219776aa6fd
Aboba <eirikrinnrauthr@tutanota.com>
2024-06-07 21:48:02 +0100

    Adjust tmux scripts for myself and pipe result into less for sessionizer

--- .local/scripts/tmux-cht.sh
+++ .local/scripts/tmux-cht.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
-selected=`cat ~/.config/tmux/.tmux-cht-languages ~/.config/tmux/.tmux-cht-command | fzf`
+
+selected=`cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | fzf`
+
 if [[ -z $selected ]]; then
     exit 0
 fi
 
 read -p "Enter Query: " query
 
-if grep -qs "$selected" ~/.config/tmux/.tmux-cht-languages; then
+if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 else
     tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi
